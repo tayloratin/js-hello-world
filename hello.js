6eb7f9e607eb2d2f1b337769b7f1c6dc9b5d3064
Taylor Tinnes <tayloratinnes@gmail.com>
2024-05-25 23:15:26 -0500

    git status git status On branch main Your branch is ahead of 'origin/main' by 1 commit. (use "git push" to publish your local commits)
    Changes not staged for commit:
    (use "git add <file>..." to update what will be committed)
    (use "git restore <file>..." to discard changes in working directory)
        modified:   hello.js
    
    Untracked files:
        (use "git add <file>..." to include in what will be committed)
            package-lock.json
    
    no changes added to commit (use "git add" and/or "git commit -a
    
    git add.
    Git status

--- hello.js
+++ hello.js
@@ -4,12 +4,10 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log('Hello, World!')
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
-
    1. Change the message that is printed.
    2. Figure out what the parentheses do. Will the code work without them?
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
